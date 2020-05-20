13a0cee2fb0e8f37bebf45a3a1eadd4474058b8f
Narek Martirosyan <narek.mp@gmail.com>
2020-05-20 11:01:14 +0200

    pending edit ??

--- C++/test/Source.cpp
+++ C++/test/Source.cpp
@@ -2,33 +2,6 @@
 #include <time.h>
 using namespace std;
 
-int main() {
+void main(int argc, char* argv[]) {
 
-	srand(time(NULL));
-
-	int rows = 4;
-	int columns = 5;
-
-	int** arr = new int* [rows];
-	//arr[0] -> array
-
-	for (int i = 0; i < rows; i++) {
-		arr[i] = new int[columns];
-	}
-
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < columns; j++) {
-			arr[i][j] = rand() % 15;
-			cout << arr[i][j] << '\t';
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < rows; i++) {
-		delete [] arr[i];
-	}
-	
-	delete[] arr;
-
-	return 0;
 }
